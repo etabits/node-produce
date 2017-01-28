0cab792e7e61b30ee65f9cedb9c7121a2723b944
Hasan Arous <arous.hasan@gmail.com>
2017-01-28 03:51:15 +0200

    A minimal HTTP Target

---
+++ src/HTTPTarget.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const http = require('http')
+
+class HTTPTarget {
+  constructor (opts) {
+    this.opts = opts
+    this.server = http.createServer(this.requestHandler.bind(this))
+  }
+
+  requestHandler (req, res) {
+    this.produce.process({
+      output: {
+        relPath: req.url
+      }
+    })
+    .then(function (result) {
+      res.end(result.output.data)
+    })
+    .catch(error => console.error(error))
+  }
+
+  run () {
+    this.server.listen(this.opts.port)
+    console.log(`Listening at http://127.0.0.1:${this.opts.port}/`)
+  }
+}
+
+module.exports = HTTPTarget
