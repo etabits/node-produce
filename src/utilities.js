6b51bc58c3c4c7b587341f3046e35ff492e8eac8
Hasan Arous <arous.hasan@gmail.com>
2017-01-30 13:04:11 +0200

    Put function outside loop!
    jshint error: Don't make functions within a loop. (W083)

--- src/utilities.js
+++ src/utilities.js
@@ -25,7 +25,7 @@ utilities.autoHandlers.sourceTargets = function (source) {
   for (var i = 0, len = this.source.length; i < len; ++i) {
     if (source.endsWith(this.source[i])) {
       var basename = source.substr(0, source.length - this.source[i].length)
-      return this.target.map(t => basename + t)
+      return this.target.map(addSuffix(basename))
     }
   }
   return null
@@ -34,10 +34,12 @@ utilities.autoHandlers.targetSources = function (target) {
   for (var i = 0, len = this.target.length; i < len; ++i) {
     if (target.endsWith(this.target[i])) {
       var basename = target.substr(0, target.length - this.target[i].length)
-      return this.source.map(s => basename + s)
+      return this.source.map(addSuffix(basename))
     }
   }
   return null
 }
+
+var addSuffix = (basename) => (suffix) => basename + suffix
 
 module.exports = utilities
