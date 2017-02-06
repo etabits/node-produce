b2d5e6bc7aa68e3359b5b20ec0654b0fa9eff2f1
Hasan Arous <arous.hasan@gmail.com>
2017-02-06 15:35:24 +0200

    Export fs source/target classes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict'
 
 exports.Produce = require('./src/Produce')
+exports.FileSystemSource = require('./src/FileSystemSource')
+exports.FileSystemTarget = require('./src/FileSystemTarget')
 exports.HTTPTarget = require('./src/HTTPTarget')
